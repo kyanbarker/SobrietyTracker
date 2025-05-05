2b1dc0d142416e200dbd72d620ab89bab533b044
Kyan Barker <94871674+kyanbarker@users.noreply.github.com>
2025-05-05 11:10:00 -0400

    Added engagements

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
+import { differenceInDays, max } from "date-fns";
 import { useState } from "react";
 import Calendar from "./components/Calendar";
 
 function App() {
   const [selectedDay, setSelectedDay] = useState<Date | null>(null);
+  const engagements = [
+    new Date(2025, 4, 3),
+    new Date(2025, 4, 1),
+    new Date(2025, 3, 16),
+    new Date(2025, 3, 17),
+    new Date(2025, 3, 18),
+    new Date(2025, 3, 20),
+  ];
+  const lastEngagement = max(engagements);
+  const numDaysSinceLastEngagement = differenceInDays(
+    new Date(),
+    lastEngagement
+  );
   return (
     <div className="p-8">
       <Calendar
         initialDate={new Date(2025, 4)}
         onClick={(day) => setSelectedDay(day)}
-        datesToHighlight={[
-          new Date(2025, 4, 3),
-          new Date(2025, 4, 1),
-          new Date(2025, 3, 16),
-          new Date(2025, 3, 17),
-          new Date(2025, 3, 18),
-          new Date(2025, 3, 20),
-        ]}
+        datesToHighlight={engagements}
       />
       <h2>{selectedDay?.toDateString()}</h2>
+      <h2>
+        Last Engagement: {lastEngagement.toDateString()} (
+        {numDaysSinceLastEngagement} Days Ago)
+      </h2>
     </div>
   );
 }
